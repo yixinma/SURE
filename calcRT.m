701769d3c44588e655fa897bcee51267b9053a82
yixinma <yixinma@umich.edu>
2015-05-22 15:29:44 -0400

    The copied original calculation file.

---
+++ calcRT.m
@@ -0,0 +1,51 @@
+%prompt='Enter the trajectory of the point: ';
+%track=input(prompt);
+track=[1 2 3; 2 3 4; 3 4 5; 4 5 6; 5 6 7; 6 7 8; 7 8 9];
+t=size(track,1);
+
+A=zeros((3*(t-1)),12);
+for i=1:(t-1)
+    for j=1:3
+        dim1=3*(i-1)+j;
+        for k=1:3
+            dim2=3*(j-1)+k;
+            A(dim1,dim2)=track(i,k);
+        end
+        dim3=9+j;
+        A(dim1,dim3)=1;
+    end
+end
+
+b=zeros((3*(t-1)),1);
+for i=2:t
+    for j=1:3
+        dim4=3*(i-2)+j;
+        b(dim4,1)=track(i,j);
+    end
+end
+
+X=lscov(A,b);
+
+R=zeros(3,3);
+for i=1:3
+    for j=1:3
+        R(i,j)=X(3*(i-1)+j,1);
+    end
+end
+
+T=zeros(3,1);
+for i=1:3
+    T(i)=X(9+i,1);
+end
+
+R
+T
+
+%testing part
+time1=[1; 2; 3];
+time2=R*test1+T
+time3=R*test2+T
+time4=R*test3+T
+time5=R*test4+T
+time6=R*test5+T
+time7=R*test6+T
